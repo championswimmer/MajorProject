c1a2731415aa29c0ea5a1a1b7ed5e73ee086de72
Arnav Gupta <championswimmer@gmail.com>
2015-12-04 02:06:48 +0530

    layout fixes
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- client_webapp/app/rooms/guestroom/GuestroomController.js
+++ client_webapp/app/rooms/guestroom/GuestroomController.js
@@ -2,7 +2,9 @@
  * Created by championswimmer on 3/12/15.
  */
 "use strict";
-var guestroomModule = angular.module('hi.guestroom', ['ui.router']);
+var guestroomModule = angular.module('hi.guestroom', [
+    'ui.router'
+]);
 
 guestroomModule.config(['$stateProvider', function($stateProvider) {
     $stateProvider.state('guestroom', {
@@ -13,6 +15,29 @@ guestroomModule.config(['$stateProvider', function($stateProvider) {
 }]);
 
 guestroomModule.controller('GuestroomController',
-    ['$mdDialog', '$rootScope', function($mdDialog, $rootScope) {
+    ['$mdDialog', '$rootScope', 'SwitchService', function($mdDialog, $rootScope, SwitchService) {
+        
+        var grc = this;
+
+        grc.data = {};
+        grc.switches = config.guestroom.switches;
+        for (var i = 0; i < grc.switches.length; i++) {
+
+            console.log( i + " " + JSON.stringify(grc.switches[i]));
+            grc.switches[i].state = false;
+        }
+
+        var mySwSrv = SwitchService;
+        grc.switchToggle = function (sw) {
+            console.log("Switch toggled" + sw.state);
+            mySwSrv.setRoom("guestroom");
+            mySwSrv.setAppl("led");
+            mySwSrv.setState(sw.state ? "off" : "on");
+            mySwSrv.performSwitch().then(function (data) {
+
+            }, function (data) {
+                window.alert(data);
+            });
+        };
 
     }]);
--- client_webapp/app/rooms/hall/HallController.js
+++ client_webapp/app/rooms/hall/HallController.js
@@ -2,7 +2,9 @@
  * Created by championswimmer on 3/12/15.
  */
 "use strict";
-var hallModule = angular.module('hi.hall', ['ui.router']);
+var hallModule = angular.module('hi.hall', [
+    'ui.router'
+]);
 
 hallModule.config(['$stateProvider', function($stateProvider) {
     $stateProvider.state('hall', {
@@ -13,6 +15,34 @@ hallModule.config(['$stateProvider', function($stateProvider) {
 }]);
 
 hallModule.controller('HallController',
-    ['$mdDialog', '$rootScope', function($mdDialog, $rootScope) {
+    ['$mdDialog', '$rootScope', 'SwitchService', function($mdDialog, $rootScope, SwitchService) {
+        
+        var hc = this;
+        hc.data = {};
+        hc.switches = config.hall.switches;
+        for (var i = 0; i < hc.switches.length; i++) {
+
+            console.log( i + " " + JSON.stringify(hc.switches[i]));
+            hc.switches[i].state = false;
+        }
+
+        hc.color  = {
+            red: Math.floor(Math.random() * 255),
+            green: Math.floor(Math.random() * 255),
+            blue: Math.floor(Math.random() * 255)
+        };
+
+        var mySwSrv = SwitchService;
+        hc.switchToggle = function (sw) {
+            console.log("Switch toggled" + sw.state);
+            mySwSrv.setRoom("bedroom");
+            mySwSrv.setAppl("led");
+            mySwSrv.setState(sw.state ? "off" : "on");
+            mySwSrv.performSwitch().then(function (data) {
+
+            }, function (data) {
+                window.alert(data);
+            });
+        };
 
     }]);
